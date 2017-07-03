b22747102b0d28a54dcf6789f4b2fd3c38a9efbf
ArtanisW <wjqwonder@hotmail.com>
2017-07-03 23:48:35 +0800

    Add views
    Add some views demo

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ module.exports = function (app) {
     app.use('/login', require('./login'));
     app.use('/logout', require('./logout'));
     app.use('/home', require('./home'));
+    app.use('/tab2', require('./tab2'));
+    app.use('/tab3', require('./tab3'));
     app.use(function (req, res) {
         if (!res.headerSent) {
             res.status(404).render('404');
