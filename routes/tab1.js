9eb6c2ea7fd96fe5c7c60cce706819b38d9f198d
ArtanisW <wjqwonder@hotmail.com>
2017-07-04 17:11:50 +0800

    Add some comments
    Add some comments

--- routes/tab1.js
+++ routes/tab1.js
@@ -3,16 +3,17 @@ const router = express.Router();
 //route层引用了model层
 const BasicInfoModel = require('../models/BasicInfo');
 const checkLogin = require('../middlewares/check').checkLogin;
-
+//进入目录时进行渲染
 router.get('/', checkLogin, function (req, res, next) {
     res.render('tab1');
 });
+//查询接口
 router.get('/getInfo', checkLogin, function (req, res, next) {
     const uid = req.session.user._id;
     BasicInfoModel.getBasicInfoByUid(uid)
         .then(function (basicInfo) {
             if (!basicInfo) {
-                req.flash('error','用户信息不存在，请填写并保存');
+                // req.flash('error','用户信息不存在，请填写并保存');
             }else {
                 var resJSON = {
                     code: 0,
@@ -28,6 +29,7 @@ router.get('/getInfo', checkLogin, function (req, res, next) {
             res.send(resJSON);
         })
 });
+//新建接口
 router.post('/create', checkLogin, function (req, res, next) {
     const basicInfo = {
         uid: req.session.user._id,
@@ -44,13 +46,14 @@ router.post('/create', checkLogin, function (req, res, next) {
     res.send(resJSON);
 
 });
+//更新接口
 router.post('/update', checkLogin, function (req, res, next) {
     const uid = req.session.user._id;
     const basicInfo = {
         id: req.body.id,
         className: req.body.className,
         name: req.body.name,
-        groupName: req.body.groupName,
+        groupName: req.body.groupName
     };
     BasicInfoModel.updateBasicInfoByUid(uid, basicInfo);
     const resJSON = {
